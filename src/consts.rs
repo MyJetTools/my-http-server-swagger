acf92ba153f723cbd2d808bb3bdf5244d3bf4451
amigin <amigin@gmail.com>
2022-04-19 16:00:53 +0300

    Fixing HTTP_DATA_TYPE

--- src/consts.rs
+++ src/consts.rs
@@ -27,7 +27,7 @@ pub const HTTP_ARRAY_ELEMENT: &str =
     "my_http_server_controllers::controllers::documentation::data_types::ArrayElement";
 
 pub const HTTP_DATA_TYPE: &str =
-    "data_type: my_http_server_controllers::controllers::documentation::data_types::HttpDataType";
+    "my_http_server_controllers::controllers::documentation::data_types::HttpDataType";
 
 pub const HTTP_ACTION_DESCRIPTION: &str =
     "my_http_server_controllers::controllers::documentation::HttpActionDescription";
