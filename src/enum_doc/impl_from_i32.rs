f721c8b221c486cd674f16630d0b53240cd13ba7
amigin <amigin@gmail.com>
2022-11-01 20:56:36 +0200

    Debugging

--- src/enum_doc/impl_from_i32.rs
+++ src/enum_doc/impl_from_i32.rs
@@ -5,11 +5,23 @@ pub fn generate(enum_cases: &[EnumJson]) -> String {
 
     result.push_str("match src {");
 
+    let mut default = None;
+
     for enum_case in enum_cases {
+        if enum_case.has_default_attr() {
+            default = Some(enum_case.get_enum_case_value());
+        }
         result.push_str(enum_case.get_id().to_string().as_str());
         result.push_str(" => Self::");
         result.push_str(enum_case.get_enum_case_value());
         result.push(',');
+    }
+
+    if let Some(default) = default {
+        result.push_str("_ => Self::");
+        result.push_str(default);
+    } else {
+        result.push_str("_ => panic!(\"Can not parse enum with value {}\", src)");
     }
 
     result.push_str("}");
