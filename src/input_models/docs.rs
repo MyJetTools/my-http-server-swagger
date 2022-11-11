48cb51b26d826689580644de6f52b16ac86ed40c
amigin <amigin@gmail.com>
2022-11-12 00:07:47 +0200

    Fixing

--- src/input_models/docs.rs
+++ src/input_models/docs.rs
@@ -57,6 +57,7 @@ fn get_input_src(field: &InputField) -> String {
         InputFieldSource::Header => "Header",
         InputFieldSource::Body => "Body",
         InputFieldSource::Form => "FormData",
+        InputFieldSource::BodyFile => "BodyFile",
     };
 
     return format!("{NAME_SPACE}::{HTTP_PARAMETER_INPUT_SRC}::{field}",);
