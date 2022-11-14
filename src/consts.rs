fc7b96c4fba61ad9634e8f00a0c9ce7add0ad314
amigin <amigin@gmail.com>
2022-11-14 09:59:21 +0200

    Fixes

--- src/consts.rs
+++ src/consts.rs
@@ -28,7 +28,7 @@ pub const HTTP_OK_RESULT: &str = "my_http_server::HttpOkResult";
 
 pub const HTTP_SIMPLE_TYPE: &str = "data_types::HttpSimpleType";
 
-pub const HTTP_ARRAY_ELEMENT: &str = "ArrayElement";
+pub const HTTP_ARRAY_ELEMENT: &str = "data_types::ArrayElement";
 
 pub const HTTP_DATA_TYPE: &str = "data_types::HttpDataType";
 
