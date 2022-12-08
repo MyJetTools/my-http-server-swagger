8d6859b0804ca95b02c8c41ad4bd9c551dc26cfc
amigin <amigin@gmail.com>
2022-12-08 18:31:19 +0200

    Debugging

--- src/input_models/model_reader/model_reader.rs
+++ src/input_models/model_reader/model_reader.rs
@@ -23,6 +23,7 @@ pub fn generate(result: &mut String, name: &str, input_fields: &InputFields) {
             BodyDataToReader::BodyFile => {}
             BodyDataToReader::RawBodyToVec => {}
             BodyDataToReader::BodyModel => {
+                println!("Going thorugh reading BodyModel");
                 result.push_str("let __body = ctx.request.get_body().await?;");
                 super::generate_read_body(
                     result,
