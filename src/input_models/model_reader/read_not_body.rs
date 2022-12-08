a1a1de2ffe2511287e5e187a4d25283015a04aa0
amigin <amigin@gmail.com>
2022-12-08 21:09:10 +0200

    fixing

--- src/input_models/model_reader/read_not_body.rs
+++ src/input_models/model_reader/read_not_body.rs
@@ -36,7 +36,7 @@ pub fn generate_read_not_body(result: &mut String, input_fields: &InputFields) {
                 result.push_str(input_field.name());
                 result.push_str("\"){");
                 result.push_str(
-                    "let value = my_http_server::InputParamValue::from(value);Some(value.try_into()?)}else{None};",
+                    "let value = my_http_server::InputParamValue::from(value);Some(value.parse()?)}else{None};",
                 );
             }
             PropertyType::VecOf(sub_type) => {
