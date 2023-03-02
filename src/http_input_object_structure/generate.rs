7035f016a4a358292b3b6dbb0391ea543ffd8fd5
amigin <amigin@gmail.com>
2023-03-02 09:08:10 +0200

    Fixes

--- src/http_input_object_structure/generate.rs
+++ src/http_input_object_structure/generate.rs
@@ -1,29 +1,69 @@
-use quote::quote;
+use std::str::FromStr;
+
+use quote::{quote, ToTokens};
 use types_reader::StructProperty;
 
 use super::struct_prop_ext::SturctPropertyExt;
 
 pub fn generate(ast: &syn::DeriveInput) -> proc_macro::TokenStream {
     let stuct_name = &ast.ident;
+    let generic = &ast.generics;
 
-    quote! {
-        impl<'s> TryFrom<my_http_server::InputParamValue<'s>> for #stuct_name {
-            type Error = my_http_server::HttpFailResult;
+    let (generic, generic_ident) = if generic.params.is_empty() {
+        (None, None)
+    } else {
+        let generic_ident = generic.params.to_token_stream().to_string();
+        let generic_ident_pos = generic_ident.find(':').unwrap();
 
-            fn try_from(value: my_http_server::InputParamValue) -> Result<Self, Self::Error> {
-                value.from_json()
+        let gen = &generic_ident.as_bytes()[..generic_ident_pos];
+        let gen = std::str::from_utf8(gen).unwrap();
+        println!("generic_ident: {}", gen);
+
+        let generic_ident = proc_macro2::TokenStream::from_str(gen).unwrap();
+
+        (Some(quote!(#generic)), Some(quote!(<#generic_ident>)))
+    };
+
+    let result = if let Some(generic) = generic {
+        let generic_ident = generic_ident.unwrap();
+        quote! {
+            impl<'s, #generic> TryFrom<my_http_server::InputParamValue<'s>> for #stuct_name #generic_ident {
+                type Error = my_http_server::HttpFailResult;
+
+                fn try_from(value: my_http_server::InputParamValue) -> Result<Self, Self::Error> {
+                    value.from_json()
+                }
+            }
+
+            impl #generic TryFrom<my_http_server::HttpRequestBody> for #stuct_name #stuct_name #generic_ident {
+                type Error = my_http_server::HttpFailResult;
+
+                fn try_from(value: my_http_server::HttpRequestBody) -> Result<Self, Self::Error> {
+                    value.get_body_as_json()
+                }
             }
         }
+    } else {
+        quote! {
+            impl<'s> TryFrom<my_http_server::InputParamValue<'s>> for #stuct_name {
+                type Error = my_http_server::HttpFailResult;
 
-        impl TryFrom<my_http_server::HttpRequestBody> for #stuct_name {
-            type Error = my_http_server::HttpFailResult;
+                fn try_from(value: my_http_server::InputParamValue) -> Result<Self, Self::Error> {
+                    value.from_json()
+                }
+            }
 
-            fn try_from(value: my_http_server::HttpRequestBody) -> Result<Self, Self::Error> {
-                value.get_body_as_json()
+            impl TryFrom<my_http_server::HttpRequestBody> for #stuct_name {
+                type Error = my_http_server::HttpFailResult;
+
+                fn try_from(value: my_http_server::HttpRequestBody) -> Result<Self, Self::Error> {
+                    value.get_body_as_json()
+                }
             }
         }
-    }
-    .into()
+    };
+
+    result.into()
 }
 
 pub fn generate_http_object_structure(
