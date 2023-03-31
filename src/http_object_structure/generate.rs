658a93fa943932b91f02ec6cc9ed17334347d848
amigin <amigin@gmail.com>
2023-03-31 15:35:39 +0300

    fixes

--- src/http_object_structure/generate.rs
+++ src/http_object_structure/generate.rs
@@ -66,7 +66,7 @@ pub fn generate(ast: &syn::DeriveInput) -> (proc_macro::TokenStream, bool) {
             }
         }
 
-        impl<'s, #generic_no_brackets> TryInto<#struct_name> for my_http_server::InputParamValue<'s, #generic_ident_no_brackets> {
+        impl<'s, #generic_no_brackets> TryInto<#struct_name #generic_ident> for my_http_server::InputParamValue<'s, #generic_ident_no_brackets> {
             type Error = my_http_server::HttpFailResult;
         
             fn try_into(self) -> Result<#struct_name, Self::Error> {
