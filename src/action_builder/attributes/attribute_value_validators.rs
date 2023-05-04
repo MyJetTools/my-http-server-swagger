78de10382f5af082118a48e89e5d020c06313f94
amigin <amigin@gmail.com>
2023-05-04 15:52:40 +0300

    Removed Verification

--- src/action_builder/attributes/attribute_value_validators.rs
+++ src/action_builder/attributes/attribute_value_validators.rs
@@ -4,10 +4,10 @@ pub fn validate_authorized_attribute_value(
     token_stream: &proc_macro::TokenStream,
     value: &str,
 ) -> Result<(), syn::Error> {
-    if !is_array_of_strings(value) {
-        let ast: DeriveInput = syn::parse(token_stream.clone()).unwrap();
-        return Err(syn::Error::new_spanned(ast, format!("The value of the attribute 'authorized' must be an array of strings. Example: ['admin', 'user'] er Empty array: Example: []. Or \"global\" or \"no\"")));
-    }
+    //if !is_array_of_strings(value) {
+    //    let ast: DeriveInput = syn::parse(token_stream.clone()).unwrap();
+    //    return Err(syn::Error::new_spanned(ast, format!("The value of the attribute 'authorized' must be an array of strings. Example: ['admin', 'user'] er Empty array: Example: []. Or \"global\" or \"no\"")));
+    // }
 
     Ok(())
 }
