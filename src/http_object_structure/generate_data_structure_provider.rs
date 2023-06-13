12ebb5fc500ce84bda5af3b94ee9fdbe7dd62967
amigin <amigin@gmail.com>
2023-06-13 15:54:19 +0300

    Debugging

--- src/http_object_structure/generate_data_structure_provider.rs
+++ src/http_object_structure/generate_data_structure_provider.rs
@@ -14,7 +14,7 @@ pub fn generate_data_structure_provider(
     let (generic, generic_ident, generic_param) = if let Some(generic) = GenericData::new(ast) {
         let generic_token_stream = generic.generic;
         let generic_ident = generic.generic_ident;
-        let generic_param = generic_ident.to_string();
+        let generic_param = generic_token_stream.to_string();
         (
             generic_token_stream,
             generic_ident,
