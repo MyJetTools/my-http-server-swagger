b9e975ec06b5ecffbd8730195b53fa44b4b218ea
amigin <amigin@gmail.com>
2023-06-13 16:42:08 +0300

    Debugging

--- src/types.rs
+++ src/types.rs
@@ -26,10 +26,10 @@ fn compile_data_type(pt: &PropertyType) -> TokenStream {
     if let PropertyType::OptionOf(generic_type) = pt {
         let type_token = generic_type.get_token_stream_with_generics();
 
-        return quote!(#type_token::get_data_type());
+        return quote!(#type_token::get_data_type(None));
     }
 
     let type_token = pt.get_token_stream_with_generics();
 
-    return quote!(#type_token::get_data_type());
+    return quote!(#type_token::get_data_type(None));
 }
